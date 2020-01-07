86aa5a9ba0d2912e027825f234b836e975491652
Akkrapol Kanpong <53117897+touchmylike2019@users.noreply.github.com>
2020-01-07 08:36:12 +0700

    Updates

---
+++ precache-manifest.4e26dfc0eed868f070f21e44b6f9080e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab533abf7f4230374174691ccd4db8da",
+    "url": "/React_CRUD/index.html"
+  },
+  {
+    "revision": "36a630d74549e2b54851",
+    "url": "/React_CRUD/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "36a630d74549e2b54851",
+    "url": "/React_CRUD/static/js/2.9c1cf226.chunk.js"
+  },
+  {
+    "revision": "baf0d6b37b8977398e02",
+    "url": "/React_CRUD/static/js/main.09410949.chunk.js"
+  },
+  {
+    "revision": "058b4f5e43fb984bbd59",
+    "url": "/React_CRUD/static/js/runtime~main.0fbe32fe.js"
+  }
+]);
